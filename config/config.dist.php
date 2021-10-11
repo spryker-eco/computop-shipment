158adf746b6118f5317e1f2709d1cb5b8170917f
Kostiantyn Kichehlov <kostiantyn.kichehlov@spryker.com>
2021-10-11 10:24:58 +0300

    ECO-3658 Add base dir structure + files

---
+++ config/config.dist.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Copy over the following configs to your config
+ */
+
+use SprykerEco\Shared\ComputopShipment\ComputopShipmentConstants;
+
+$config[ComputopShipmentConstants::PAYPAL_EXPRESS_DEFAULT_SHIPMENT_METHOD_ID] = 1;
---
+++ src/SprykerEco/Shared/ComputopShipment/ComputopShipmentConstants.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace SprykerEco\Shared\ComputopShipment;
+
+interface ComputopShipmentConstants
+{
+    /**
+     * @var string
+     */
+    public const PAYPAL_EXPRESS_DEFAULT_SHIPMENT_METHOD_ID = 'COMPUTOP_SHIPMENT:PAYPAL_EXPRESS_DEFAULT_SHIPMENT_METHOD_ID';
+}
